ae1451569d15edcdfdef63ed4582e1ea94a4296e
Nello-Moreira <g.moreira1@outlook.com>
2021-09-07 17:05:12 -0300

    changed: button background color

--- src/components/buttons/BlueButton.js
+++ src/components/buttons/BlueButton.js
@@ -13,7 +13,11 @@ const Button = styled.button`
     color: rgb(255, 255, 255);
     width: ${({ customStyle }) => customStyle === 'login' ? '90%' : 'auto'};
     height: ${({ customStyle }) => customStyle === 'login' ? '45px' : '35px'};
-    background-color:rgba(82, 182, 255, 1);
+    background-color: rgba(18, 107, 165, 1);
     border: none;
     border-radius: 5px;
+
+    :hover{
+        background-color: rgba(82, 182, 255, 1);
+    }
 `;
